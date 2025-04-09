39a75254ecf9d1eb5940d34eba84918c53a69e61
Altamash Rashid <asr.designmaster@gmail.com>
2025-04-09 17:04:17 +0530

    info edit fixed

--- resources/views/backend/properties/popup_forms/property_info.blade.php
+++ resources/views/backend/properties/popup_forms/property_info.blade.php
@@ -25,65 +25,74 @@
         <input type="hidden" name="property_id" value="{{ $property->id }}">
         <input type="hidden" name="form_type" value="property_info">
 
-        <div class="form-group pt_wrapper">
-            <div class="rs_radio_btns">
-                <div>
-                    <input type="radio" name="property_type" id="property_type_sales" value="sales" {{ (isset($property) && $propertyType == 'sales') ? 'checked' : '' }} required />
-                    <label for="property_type_sales">Sales</label>
-                </div>
-                <div>
-                    <input type="radio" name="property_type" id="property_type_lettings" value="lettings" {{ (isset($property) && $propertyType == 'lettings') ? 'checked' : '' }} required />
-                    <label for="property_type_lettings">Lettings</label>
-                </div>
-                <div>
-                    <input type="radio" name="property_type" id="property_type_both" value="both" {{ (isset($property) && $propertyType == 'both') ? 'checked' : '' }} required />
-                    <label for="property_type_both">Both</label>
+        <div class="info_edit_form">
+            <div class="form-group pt_wrapper">
+                <div class="accordion_inner_heading mb-2">Proeperty Type</div>
+                <div class="rounded_check_btn">
+                    <div class="rs_radio_btns">
+                        <div>
+                            <input class="hidden" type="radio" name="property_type" id="property_type_sales" value="sales" {{ (isset($property) && $propertyType == 'sales') ? 'checked' : '' }} required />
+                            <label class="checkbox_btn" for="property_type_sales">Sales</label>
+                        </div>
+                        <div>
+                            <input class="hidden" type="radio" name="property_type" id="property_type_lettings" value="lettings" {{ (isset($property) && $propertyType == 'lettings') ? 'checked' : '' }} required />
+                            <label class="checkbox_btn" for="property_type_lettings">Lettings</label>
+                        </div>
+                        <div>
+                            <input class="hidden" type="radio" name="property_type" id="property_type_both" value="both" {{ (isset($property) && $propertyType == 'both') ? 'checked' : '' }} required />
+                            <label class="checkbox_btn" for="property_type_both">Both</label>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label>Transaction Type</label>
-            <div class="rs_radio_btns">
-                <div>
-                    <input type="radio" name="transaction_type" id="transaction_type_residential" value="residential" {{ (isset($property) && $transactionType == 'residential') ? 'checked' : '' }} required />
-                    <label for="transaction_type_residential">Residential</label>
-                </div>
-                <div>
-                    <input type="radio" name="transaction_type" id="transaction_type_commercial" value="commercial" {{ (isset($property) && $transactionType == 'commercial') ? 'checked' : '' }} required />
-                    <label for="transaction_type_commercial">Commercial</label>
+            <div class="form-group">
+                <div class="accordion_inner_heading mb-2">Transaction Type</div>
+                <div class="rounded_check_btn">
+                    <div class="rs_radio_btns">
+                        <div>
+                            <input class="hidden" type="radio" name="transaction_type" id="transaction_type_residential" value="residential" {{ (isset($property) && $transactionType == 'residential') ? 'checked' : '' }} required />
+                            <label class="checkbox_btn" for="transaction_type_residential">Residential</label>
+                        </div>
+                        <div>
+                            <input class="hidden" type="radio" name="transaction_type" id="transaction_type_commercial" value="commercial" {{ (isset($property) && $transactionType == 'commercial') ? 'checked' : '' }} required />
+                            <label class="checkbox_btn" for="transaction_type_commercial">Commercial</label>
+                        </div>
+                    </div>
+                    @error('transaction_type')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
-            @error('transaction_type')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
 
-        <div class="form-group">
-            <label>Specific Property Type</label>
-            <div class="rs_radio_btns">
-                <div>
-                    <input type="radio" name="specific_property_type" id="specific_property_type_appartment"
-                        value="appartment" {{ (isset($property) && $specificPropertyType == 'appartment') ? 'checked' : '' }}
-                        required>
-                    <label for="specific_property_type_appartment">Appartment</label>
-                </div>
-                <div>
-                    <input type="radio" name="specific_property_type" id="specific_property_type_flat" value="flat" {{ (isset($property) && $specificPropertyType == 'flat') ? 'checked' : '' }} required>
-                    <label for="specific_property_type_flat">Flat</label>
-                </div>
-                <div>
-                    <input type="radio" name="specific_property_type" id="specific_property_type_bunglow" value="bunglow" {{ (isset($property) && $specificPropertyType == 'bunglow') ? 'checked' : '' }} required>
-                    <label for="specific_property_type_bunglow">Bunglow</label>
-                </div>
-                <div>
-                    <input type="radio" name="specific_property_type" id="specific_property_type_house" value="house" {{ (isset($property) && $specificPropertyType == 'house') ? 'checked' : '' }} required>
-                    <label for="specific_property_type_house">House</label>
+            <div class="form-group">
+                <div class="accordion_inner_heading mb-2">Specific Property Type </div>
+                <div class="rounded_check_btn">
+                    <div class="rs_radio_btns">
+                        <div>
+                            <input class="hidden" type="radio" name="specific_property_type" id="specific_property_type_appartment"
+                                value="appartment" {{ (isset($property) && $specificPropertyType == 'appartment') ? 'checked' : '' }}
+                                required>
+                            <label class="checkbox_btn" for="specific_property_type_appartment">Appartment</label>
+                        </div>
+                        <div>
+                            <input class="hidden" type="radio" name="specific_property_type" id="specific_property_type_flat" value="flat" {{ (isset($property) && $specificPropertyType == 'flat') ? 'checked' : '' }} required>
+                            <label class="checkbox_btn" for="specific_property_type_flat">Flat</label>
+                        </div>
+                        <div>
+                            <input class="hidden" type="radio" name="specific_property_type" id="specific_property_type_bunglow" value="bunglow" {{ (isset($property) && $specificPropertyType == 'bunglow') ? 'checked' : '' }} required>
+                            <label class="checkbox_btn" for="specific_property_type_bunglow">Bunglow</label>
+                        </div>
+                        <div>
+                            <input class="hidden" type="radio" name="specific_property_type" id="specific_property_type_house" value="house" {{ (isset($property) && $specificPropertyType == 'house') ? 'checked' : '' }} required>
+                            <label class="checkbox_btn" for="specific_property_type_house">House</label>
+                        </div>
+                    </div>
+                    @error('specific_property_type')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
-            @error('specific_property_type')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
         </div>
 
         <button type="submit" class="btn btn-success mt-3 float-end">Save Changes</button>
