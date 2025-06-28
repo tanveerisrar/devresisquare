90005831d7f52dd5b18f39d28328f1d0594a3afb
Altamash Rashid <asr.designmaster@gmail.com>
2025-06-28 16:10:18 +0530

    pricing book a demo added

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -41,41 +41,41 @@
 @endphp
 
 @section('content')
-<section class="hero_section px-4">
+    <section class="hero_section px-4">
         <div class="main_hero_section">
-            <div class=""> 
-                <div class="hero_title">Revolutionize Your <br/>Property Management <br>
+            <div class="">
+                <div class="hero_title">Revolutionize Your <br />Property Management <br>
                     <span> Experience</span>
                 </div>
-                <p class="hero_description">Streamline operations, enhance tenant satisfaction, and maximize returns with ResiSquare's innovative property management solutions.</p>
+                <p class="hero_description">Streamline operations, enhance tenant satisfaction, and maximize returns with
+                    ResiSquare's innovative property management solutions.</p>
                 <div class="hero_btns">
                     <button id="hero_get_started_btn" class="btn btn_primary">Get Started</button>
                     {{-- <button class="btn btn_secondary">Book a Demo</button> --}}
                     <!-- Trigger Button -->
-                    <button type="button" id="hero_book_demo_btn" class="btn btn_secondary" data-bs-toggle="modal" data-bs-target="#bookDemoModal">
+                    <button type="button" id="hero_book_demo_btn" class="btn btn_secondary" data-bs-toggle="modal"
+                        data-bs-target="#bookDemoModal">
                         Book a Demo
                     </button>
 
                     <!-- Modal -->
-                    <div class="modal fade" id="bookDemoModal" tabindex="-1" aria-labelledby="bookDemoModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content border-0 shadow-lg rounded-4">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="bookDemoModalLabel">Book a Demo</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
+                    <div class="modal fade" id="bookDemoModal" tabindex="-1" aria-labelledby="bookDemoModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content border-0 shadow-lg rounded-4">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="bookDemoModalLabel">Book a Demo</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
 
-                        <div class="modal-body">
-                            {{-- Include your reusable form component --}}
-                            <x-form-component
-                                action="{{ route('form.submit', 'book_demo') }}"
-                                formId="bookDemoForm"
-                                submitText="Book Now"
-                                successMessage="Thank you! We will contact you shortly."
-                            />
+                                <div class="modal-body">
+                                    {{-- Include your reusable form component --}}
+                                    <x-form-component action="{{ route('form.submit', 'book_demo') }}" formId="bookDemoForm"
+                                        submitText="Book Now" successMessage="Thank you! We will contact you shortly." />
+                                </div>
+                            </div>
                         </div>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
@@ -83,21 +83,21 @@
                 <img src="{{ static_asset('asset/img/hero_img.webp') }}" alt="Hero" class="hero_main_img | img-fluid">
             </div>
         </div>
-        
+
         <div class="hero_cards">
             @foreach ($hero_card as $card)
-                <div class="hero_card_item"> 
+                <div class="hero_card_item">
                     <div class="card_title">{{ $card['title'] }}</div>
                     <p>{{ $card['details'] }}</p>
                 </div>
             @endforeach
         </div>
 
-        
+
         <div class="features_list">
             <div class="text-center my-3">
                 <h2>Enable features as you grow</h2>
-        </div>
+            </div>
             <div class="carousel_wrapper">
                 <div class="carousel">
                     @foreach ($features_list as $feature)
@@ -109,72 +109,85 @@
                 </div>
             </div>
         </div>
-</section>
-<section class="features_section">
-    <div class="features_item item_left_1 bg_white">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-12 mb-md-0 mb-3">
-                    <img src="{{ asset('asset/img/hero_img.webp') }}" alt="Feature" class="features_img_left img-fluid">
-                </div>
-                <div class="col-md-6 col-12 mb-md-0 mb-3">
-                    <div class="feature_card ">
-                        <h3>Adapt the Platform to Your Business Model</h3>
-                        <p>Tailor ResiSquare to your unique operational structure. Whether you're a solo landlord or a large letting agency, our flexible system fits seamlessly into your workflow.</p>
+    </section>
+    <section class="features_section">
+        <div class="features_item item_left_1 bg_white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-12 mb-md-0 mb-3">
+                        <img src="{{ asset('asset/img/hero_img.webp') }}" alt="Feature" class="features_img_left img-fluid">
+                    </div>
+                    <div class="col-md-6 col-12 mb-md-0 mb-3">
+                        <div class="feature_card ">
+                            <h3>Adapt the Platform to Your Business Model</h3>
+                            <p>Tailor ResiSquare to your unique operational structure. Whether you're a solo landlord or a
+                                large letting agency, our flexible system fits seamlessly into your workflow.</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="features_item item_right">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-12 mb-md-0 mb-3">
-                    <div class="feature_card">
-                        <h3>Tailor Notifications & Access for Each Team Member</h3>
-                        <p>Ensure the right people get the right updates. With ResiSquare, you can fully control which team members receive specific alerts and reports. Whether it's maintenance requests, rent reminders, or tenancy updates, personalize your notification settings to enhance productivity and reduce noise.</p>
-                        <p>Empower your team with relevant information — no more, no less.</p>
+        <div class="features_item item_right">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-12 mb-md-0 mb-3">
+                        <div class="feature_card">
+                            <h3>Tailor Notifications & Access for Each Team Member</h3>
+                            <p>Ensure the right people get the right updates. With ResiSquare, you can fully control which
+                                team members receive specific alerts and reports. Whether it's maintenance requests, rent
+                                reminders, or tenancy updates, personalize your notification settings to enhance
+                                productivity and reduce noise.</p>
+                            <p>Empower your team with relevant information — no more, no less.</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-6 col-12 mb-md-0 mb-3">
-                    <img src="{{ asset('asset/img/hero_img.webp') }}" alt="Feature" class="features_img_right img-fluid">
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="features_item item_left_2 bg_white">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-12 mb-md-0 mb-3">
-                    <img src="{{ asset('asset/img/hero_img.webp') }}" alt="Feature" class="features_img_left img-fluid">
-                </div>
-                <div class="col-md-6 col-12 mb-md-0 mb-3">
-                    <div class="feature_card">
-                        <h3>Define Permissions for Better Control & Security</h3>
-                        <p>Keep your operations secure and organized with customizable role-based permissions. Assign different access levels to admins, managers, and assistants — so everyone works within their scope. Whether managing documents, financial data, or tenancy records, you're always in control of who sees what.</p>
-                        <p>Give your team the access they need, and protect what matters most.</p>
+                    <div class="col-md-6 col-12 mb-md-0 mb-3">
+                        <img src="{{ asset('asset/img/hero_img.webp') }}" alt="Feature"
+                            class="features_img_right img-fluid">
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<section class="book_demo_section">
-    <div class="container">
+        <div class="features_item item_left_2 bg_white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-12 mb-md-0 mb-3">
+                        <img src="{{ asset('asset/img/hero_img.webp') }}" alt="Feature" class="features_img_left img-fluid">
+                    </div>
+                    <div class="col-md-6 col-12 mb-md-0 mb-3">
+                        <div class="feature_card">
+                            <h3>Define Permissions for Better Control & Security</h3>
+                            <p>Keep your operations secure and organized with customizable role-based permissions. Assign
+                                different access levels to admins, managers, and assistants — so everyone works within their
+                                scope. Whether managing documents, financial data, or tenancy records, you're always in
+                                control of who sees what.</p>
+                            <p>Give your team the access they need, and protect what matters most.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="book_demo_section">
+        <div class="container">
             <div class="">
                 <h2>Experience ResiSquare Firsthand</h2>
-                <p>Discover how our platform can transform your property management tasks. Schedule a demo today and take the first step towards efficient property management.</p>
+                <p>Discover how our platform can transform your property management tasks. Schedule a demo today and take
+                    the first step towards efficient property management.</p>
             </div>
             <div class="book_demo_btn">
                 <button class="btn btn_secondary btn-lg">Book Now</button>
-             </div>
+            </div>
             <div class="book_demo_img">
                 <img src="{{ asset('asset/img/hero_img.webp') }}" alt="Book a demo" class="">
             </div>
-    </div>
-</section>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/gsap.min.js" integrity="sha512-NcZdtrT77bJr4STcmsGAESr06BYGE8woZdSdEgqnpyqac7sugNO+Tr4bGwGF3MsnEkGKhU2KL2xh6Ec+BqsaHA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/ScrollTrigger.min.js" integrity="sha512-P2IDYZfqSwjcSjX0BKeNhwRUH8zRPGlgcWl5n6gBLzdi4Y5/0O4zaXrtO4K9TZK6Hn1BenYpKowuCavNandERg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="{{ static_asset('asset/js/gsap-animation.js') }}"></script>
+        </div>
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/gsap.min.js"
+        integrity="sha512-NcZdtrT77bJr4STcmsGAESr06BYGE8woZdSdEgqnpyqac7sugNO+Tr4bGwGF3MsnEkGKhU2KL2xh6Ec+BqsaHA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/ScrollTrigger.min.js"
+        integrity="sha512-P2IDYZfqSwjcSjX0BKeNhwRUH8zRPGlgcWl5n6gBLzdi4Y5/0O4zaXrtO4K9TZK6Hn1BenYpKowuCavNandERg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{ static_asset('asset/js/gsap-animation.js') }}"></script>
 
 @endsection
--- resources/views/frontend/pricing.blade.php
+++ resources/views/frontend/pricing.blade.php
@@ -3,145 +3,165 @@
 
 
 @section('content')
-<section class="pricing_section">
-    <h2 class="mb-5">Resisquare pricing plan</h2>
+    <section class="pricing_section">
+        <h2 class="mb-5">Resisquare pricing plan</h2>
 
-    <div class="pricing_tables my-5">
-        {{-- table --}}
-        <div class="price_table">
-            <div class="price_table_header">
-                <h3>Resi Pro</h3>
-            </div>
-            <div class="price_table_content">
-                <p class="desc">Full-featured property management for landlords and self-employed agents.
-    Includes unlimited properties, document storage, tenant maintenance reporting, and full CRM access via app and desktop.</p>
-                <p class="price">£75<span>/mo</span></p>
-                <div class="pricing_features_wrapper">
-                    <ul class="pricing_features">
-                        <li>Unlimited properties</li>
-                        <li>Document storage</li>
-                        <li>Maintenance reporting via app</li>
-                        <li>Full CRM access</li>
-                    </ul>
-                </div>
-            </div>
-            <button class="btn btn_secondary pricing_btn">Get Started</button>
-        </div>
-        {{-- table end --}}
-        {{-- table --}}
-        <div class="price_table">
-            <div class="price_table_header">
-                <h3>Resi Premium</h3>
-            </div>
-            <div class="price_table_content">
-                <p class="desc">Enhanced support with a dedicated property manager. Everything in ResiLite plus personalized property management, priority issue resolution, and expert compliance guidance</p>
-                <p class="price">£150<span>/mo</span></p>
-                <div class="pricing_features_wrapper">
-                    <ul class="pricing_features">
-                        <li>All ResiPro features</li>
-                        <li>Dedicated property manager</li>
-                        <li>Priority issue resolution</li>
-                        <li>Compliance help</li>
-                    </ul>
-                </div>
-            </div>
-            <button class="btn btn_secondary pricing_btn">Get Started</button>
-        </div>
-        {{-- table end --}}
-        {{-- table --}}
-        <div class="price_table">
-            <div class="price_table_header">
-                <h3>Resi Max</h3>
-                <h5 class="mt-2">For estate agencies</h5>
-            </div>
-            <div class="price_table_content">
-                <p class="desc">Comprehensive solution for estate agencies. Multi-user access, branded communications, lead management tools, advanced dashboards, and priority support.</p>
-                <p class="price">£180<span>/mo</span></p>
-                <div class="pricing_features_wrapper">
-                    <ul class="pricing_features">
-                        <li>Multi-user access</li>
-                        <li>Branded communications</li>
-                        <li>Lead management</li>
-                        <li>Dashboards</li>
-                    </ul>
-                </div>
-            </div>
-            <button class="btn btn_secondary pricing_btn">Get Started</button>
-        </div>
-        {{-- table end --}}
-
-    </div>
-
-</section>
-
-<section class="pricing_section why_us_section">
-    <div>
-        <h2 class="my-5">Why choose Resisquare?</h2>
         <div class="pricing_tables my-5">
             {{-- table --}}
-            <div class="price_table why_us_table">
+            <div class="price_table">
                 <div class="price_table_header">
-                    <h3>All-inclusive</h3>
+                    <h3>Resi Pro</h3>
                 </div>
                 <div class="price_table_content">
-                    <div>
-                        <img src="{{ static_asset('asset/img/icons/no-hidden-fees.svg')}}" alt="">
-                        <p class="desc">No hidden fees</p>
-                    </div>
-                    <div>
-                        <img src="{{ static_asset('asset/img/icons/full_access.svg')}}" alt="">
-                        <p class="desc">Full platform access</p>
+                    <p class="desc">Full-featured property management for landlords and self-employed agents.
+                        Includes unlimited properties, document storage, tenant maintenance reporting, and full CRM access
+                        via app and desktop.</p>
+                    <p class="price">£75<span>/mo</span></p>
+                    <div class="pricing_features_wrapper">
+                        <ul class="pricing_features">
+                            <li>Unlimited properties</li>
+                            <li>Document storage</li>
+                            <li>Maintenance reporting via app</li>
+                            <li>Full CRM access</li>
+                        </ul>
                     </div>
                 </div>
+                <button class="btn btn_secondary pricing_btn">Get Started</button>
             </div>
             {{-- table end --}}
             {{-- table --}}
-            <div class="price_table why_us_table">
+            <div class="price_table">
                 <div class="price_table_header">
-                    <h3>Easy Management</h3>
+                    <h3>Resi Premium</h3>
                 </div>
                 <div class="price_table_content">
-                    <div>
-                        <img src="{{ static_asset('asset/img/icons/streamline-lettings.svg')}}" alt="">
-                        <p class="desc">Streamline lettings</p>
-                    </div>
-                    <div>
-                        <img src="{{ static_asset('asset/img/icons/sales.svg')}}" alt="">
-                        <p class="desc">Sales</p>
-                    </div>
-                    <div>
-                        <img src="{{ static_asset('asset/img/icons/maintenance-app.svg')}}" alt="">
-                        <p class="desc">Maintenance in one app</p>
+                    <p class="desc">Enhanced support with a dedicated property manager. Everything in ResiLite plus
+                        personalized property management, priority issue resolution, and expert compliance guidance</p>
+                    <p class="price">£150<span>/mo</span></p>
+                    <div class="pricing_features_wrapper">
+                        <ul class="pricing_features">
+                            <li>All ResiPro features</li>
+                            <li>Dedicated property manager</li>
+                            <li>Priority issue resolution</li>
+                            <li>Compliance help</li>
+                        </ul>
                     </div>
                 </div>
+                <button class="btn btn_secondary pricing_btn">Get Started</button>
             </div>
             {{-- table end --}}
             {{-- table --}}
-            <div class="price_table why_us_table">
+            <div class="price_table">
                 <div class="price_table_header">
-                    <h3>Expert Support</h3>
+                    <h3>Resi Max</h3>
+                    <h5 class="mt-2">For estate agencies</h5>
                 </div>
                 <div class="price_table_content">
-                    <div>
-                        <img src="{{ static_asset('asset/img/icons/manage-property.svg')}}" alt="">
-                        <p class="desc">Dedicated property managers</p>
-                    </div>
-                    <div>
-                        <img src="{{ static_asset('asset/img/icons/plans.svg')}}" alt="">
-                        <p class="desc">Premium plans</p>
+                    <p class="desc">Comprehensive solution for estate agencies. Multi-user access, branded communications,
+                        lead management tools, advanced dashboards, and priority support.</p>
+                    <p class="price">£180<span>/mo</span></p>
+                    <div class="pricing_features_wrapper">
+                        <ul class="pricing_features">
+                            <li>Multi-user access</li>
+                            <li>Branded communications</li>
+                            <li>Lead management</li>
+                            <li>Dashboards</li>
+                        </ul>
                     </div>
                 </div>
+                <button class="btn btn_secondary pricing_btn">Get Started</button>
             </div>
             {{-- table end --}}
+
+        </div>
+
+    </section>
+
+    <section class="pricing_section why_us_section">
+        <div>
+            <h2 class="my-5">Why choose Resisquare?</h2>
+            <div class="pricing_tables my-5">
+                {{-- table --}}
+                <div class="price_table why_us_table">
+                    <div class="price_table_header">
+                        <h3>All-inclusive</h3>
+                    </div>
+                    <div class="price_table_content">
+                        <div>
+                            <img src="{{ static_asset('asset/img/icons/no-hidden-fees.svg')}}" alt="">
+                            <p class="desc">No hidden fees</p>
+                        </div>
+                        <div>
+                            <img src="{{ static_asset('asset/img/icons/full_access.svg')}}" alt="">
+                            <p class="desc">Full platform access</p>
+                        </div>
+                    </div>
+                </div>
+                {{-- table end --}}
+                {{-- table --}}
+                <div class="price_table why_us_table">
+                    <div class="price_table_header">
+                        <h3>Easy Management</h3>
+                    </div>
+                    <div class="price_table_content">
+                        <div>
+                            <img src="{{ static_asset('asset/img/icons/streamline-lettings.svg')}}" alt="">
+                            <p class="desc">Streamline lettings</p>
+                        </div>
+                        <div>
+                            <img src="{{ static_asset('asset/img/icons/sales.svg')}}" alt="">
+                            <p class="desc">Sales</p>
+                        </div>
+                        <div>
+                            <img src="{{ static_asset('asset/img/icons/maintenance-app.svg')}}" alt="">
+                            <p class="desc">Maintenance in one app</p>
+                        </div>
+                    </div>
+                </div>
+                {{-- table end --}}
+                {{-- table --}}
+                <div class="price_table why_us_table">
+                    <div class="price_table_header">
+                        <h3>Expert Support</h3>
+                    </div>
+                    <div class="price_table_content">
+                        <div>
+                            <img src="{{ static_asset('asset/img/icons/manage-property.svg')}}" alt="">
+                            <p class="desc">Dedicated property managers</p>
+                        </div>
+                        <div>
+                            <img src="{{ static_asset('asset/img/icons/plans.svg')}}" alt="">
+                            <p class="desc">Premium plans</p>
+                        </div>
+                    </div>
+                </div>
+                {{-- table end --}}
+            </div>
+        </div>
+
+    </section>
+
+    <section class="trusted_by_section">
+        <h4 class="my-5">Trusted by landlords and agencies alike</h4>
+        <div class="trusted_by_wrapper">
+        </div>
+    </section>
+
+    <!-- Modal -->
+    <div class="modal fade" id="bookDemoModal" tabindex="-1" aria-labelledby="bookDemoModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content border-0 shadow-lg rounded-4">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="bookDemoModalLabel">Book a Demo</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    {{-- Include your reusable form component --}}
+                    <x-form-component action="{{ route('form.submit', 'book_demo') }}" formId="bookDemoForm"
+                        submitText="Book Now" successMessage="Thank you! We will contact you shortly." />
+                </div>
+            </div>
         </div>
     </div>
-
-</section>
-
-<section class="trusted_by_section">
-    <h4 class="my-5">Trusted by landlords and agencies alike</h4>
-    <div class="trusted_by_wrapper">
-    </div>
-
-
 @endsection
